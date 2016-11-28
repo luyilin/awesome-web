ae7964b539868762d817af0faa6ab730b267fa6f
luyilin <luyilin12@gmail.com>
2016-11-28 14:58:06 +0800

    js 重写水波纹点击效果

--- rippleClick水波纹点击效果/main.js
+++ rippleClick水波纹点击效果/main.js
@@ -1,7 +1,6 @@
 $(function () {
     $('.h-btn').on('mousedown', function (e) {
         var width = $(this).outerWidth();
-        var height = width;
         var wave = $('<i class="wave"></i>').appendTo($(this));
         var colorMap = $(this).css('backgroundColor').replace('rgb(', '').replace(')', '').split(',').map(function (i) {
             return Number(i) - 20;
@@ -10,7 +9,7 @@ $(function () {
         $(this).append(wave);
         wave.css({
             width: width,
-            height: height,
+            height: width,
             left: e.pageX - $(this).offset().left - width / 2,
             top: e.pageY - $(this).offset().top - height / 2,
             background: bgColor
